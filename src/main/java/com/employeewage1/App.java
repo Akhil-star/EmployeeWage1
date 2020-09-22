330125fa161c3409044cbccd0642ee127f226d59
Akhil-star <akhilakki1305@gmail.com>
2020-09-22 21:11:43 +0530

    MonthlyWage

--- src/main/java/com/employeewage1/App.java
+++ src/main/java/com/employeewage1/App.java
@@ -9,32 +9,13 @@ public class App
     public static void main( String[] args )
     {
     	
-        Scanner sc=new Scanner(System.in);
-    	System.out.println("Enter daily/part");
-    	int n=sc.nextInt();
-        switch(n)
-        {
-                case 1 :
-                calDailywage();
-                break;
-                case 2 :
-                addParttime();
-                break;
-		        default :
-                System.out.println("Invalid");
-                break;
-         }
+        calMonwage();
     }
-        public static void addParttime(){
-        	int wage;
-        	wage=20*8;
-        	System.out.println(wage);
-        }
-        public static void calDailywage() {
-        	int wage;
-        	wage=20*8;
-        	System.out.println(wage);
-        }
+    public static void calMonwage(){
+		int wage;
+		wage=20*8*20;
+		System.out.println(wage);
+	}
    
 }
 
