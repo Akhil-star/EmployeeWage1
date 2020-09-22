831720a81fba72d6ceb3b896644ebd3d8ed26c6e
Akhil-star <akhilakki1305@gmail.com>
2020-09-22 20:27:43 +0530

    Welcome msg

---
+++ src/main/java/com/employeewage1/App.java
@@ -0,0 +1,13 @@
+package com.employeewage1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "Welocome to Employee Wage Computation Program" );
+    }
+}
