41e7192a0309a45efc0a998e4d325b4b389c4b22
Akhil-star <akhilakki1305@gmail.com>
2020-09-22 21:08:41 +0530

    Using Swtich Case

--- src/main/java/com/employeewage1/App.java
+++ src/main/java/com/employeewage1/App.java
@@ -1,5 +1,5 @@
 package com.employeewage1;
-
+import java.util.*;
 /**
  * Hello world!
  *
@@ -8,15 +8,34 @@ public class App
 {
     public static void main( String[] args )
     {
-    	addParttime();
     	
-    }
-    	
-    	public static void addParttime(){
-         	int wage;
-         	wage=20*4;
-         	System.out.println(wage);
+        Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter daily/part");
+    	int n=sc.nextInt();
+        switch(n)
+        {
+                case 1 :
+                calDailywage();
+                break;
+                case 2 :
+                addParttime();
+                break;
+		        default :
+                System.out.println("Invalid");
+                break;
          }
+    }
+        public static void addParttime(){
+        	int wage;
+        	wage=20*8;
+        	System.out.println(wage);
+        }
+        public static void calDailywage() {
+        	int wage;
+        	wage=20*8;
+        	System.out.println(wage);
+        }
    
 }
+
 
