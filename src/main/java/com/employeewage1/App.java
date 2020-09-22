790b1beb1ae441227721c460c21df17c9321db15
Akhil-star <akhilakki1305@gmail.com>
2020-09-22 20:37:42 +0530

    Employee Attendance

--- src/main/java/com/employeewage1/App.java
+++ src/main/java/com/employeewage1/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println( "Welocome to Employee Wage Computation Program" );
+        double value = Math.floor(Math.random() * 10)%2;
+		
+    	System.out.println("Random value : "+value);
+    			
+    	if(value == 1) {
+    	System.out.println("Employee present");
+    	} else {
+    	System.out.println("Employee absent");
+    	}
     }
 }
