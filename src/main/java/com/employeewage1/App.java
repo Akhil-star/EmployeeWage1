7ed818ae6528db2c413a85f1d3b4fab9eeb2130c
Akhil-star <akhilakki1305@gmail.com>
2020-09-23 09:01:10 +0530

    monthly Wage

--- src/main/java/com/employeewage1/App.java
+++ src/main/java/com/employeewage1/App.java
@@ -6,16 +6,39 @@ import java.util.*;
  */
 public class App 
 {
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
+	public static final int EMP_RATE_HOUR=20;
+	public static final int NUM_DAYS=2;
+
     public static void main( String[] args )
     {
     	
-        calMonwage();
+    	
+    	int empHrs=0;
+    	int empWage=0;
+    	int totalWage=0;
+    	
+    	for(int day=0; day<NUM_DAYS; day++) {
+    	int empCheck =(int) Math.floor(Math.random() *10)%3;
+    	switch(empCheck) {
+        case IS_FULL_TIME:
+    		empHrs=8;
+    		break;
+        case IS_PART_TIME:
+    		empHrs=4;
+    		break;
+    	default :
+    		empHrs=0;
+    		break;
     }
-    public static void calMonwage(){
-		int wage;
-		wage=20*8*20;
-		System.out.println(wage);
-	}
+    	empWage=empHrs*EMP_RATE_HOUR;
+    	totalWage += empWage;
+    	System.out.println("Emp Wage: " +empWage);
+    } 
+	System.out.println("Total Emp Wage: " +totalWage);
+
+}
    
 }
 
