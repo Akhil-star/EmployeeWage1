4f2ab753f3c1cd1cd9558751300644b8f04cf7e4
Akhil-star <akhilakki1305@gmail.com>
2020-09-23 08:25:20 +0530

    welcome

--- src/main/java/com/employeewage1/App.java
+++ src/main/java/com/employeewage1/App.java
@@ -9,26 +9,8 @@ public class App
     public static void main( String[] args )
     {
     	
-        wageTotalDaily();
-        wageTotalPart();
+        System.out.println("Welcome to employee wage computation");
     }
-     public static void wageTotalDaily(){
-		
-		int w = 0;
-		
-		for(int i=0;i<=20;i++){
-		  w=i*8*20;
-		}
-		System.out.println(w);
-	}
-     public static void wageTotalPart() {
-    	 int w=0;
-    	 for(int i=0;i<=100;i++) {
-    		 w=i*1*20;
-    	 }
-    	 System.out.println(w);
-     }
-   
 }
 
 
